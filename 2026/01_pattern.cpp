77e78ee451df7922520fd3cb24ee127ca0bc4ba5
Sakh-balti <f23bdats1e01005@iub.edu.pk>
2026-01-03 00:29:33 +0500

    practice on pattern done

--- 2026/01_pattern.cpp
+++ 2026/01_pattern.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 int main()
 {
-    int n = 3;
-    int num = 1;
-    for (int i = 1; i <= n; i++)
+    int n = 6;
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            cout << num << " ";
-            num = num + 1;
+            cout << " ";
+        }
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << j;
+        }
+        for (int k = i; k >= 1; k--)
+        {
+            cout << k;
         }
         cout << endl;
     }
