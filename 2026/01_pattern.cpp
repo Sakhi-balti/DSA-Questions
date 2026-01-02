17bfa1901c42cc73a2ca818df8a0dd6fe65476bf
Sakh-balti <f23bdats1e01005@iub.edu.pk>
2026-01-02 22:32:10 +0500

    dsa start with pattern

---
+++ 2026/01_pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n = 3;
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << num << " ";
+            num = num + 1;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
