64e7ec9130cbb9c4b9b56b8cc285a4b5c7fb8fe7
Sakh-balti <f23bdats1e01005@iub.edu.pk>
2026-01-23 21:57:46 +0500

    optimize version of 3sum problme

--- 2026/24_threeSum.cpp
+++ 2026/24_threeSum.cpp
@@ -1,20 +1,62 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <set>
 #include <unordered_map>
+
 using namespace std;
-vector<int> twoSum(vector<int> &nums, int target)
+// Brute Force approch
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    int n = nums.size();
+    set<vector<int>> s;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    {
+                        vector<int> triplet = {nums[i], nums[j], nums[k]};
+                        sort(triplet.begin(), triplet.end());
+                        if (s.find(triplet) == s.end())
+                        {
+
+                            s.insert(triplet);
+                            ans.push_back(triplet);
+                        }
+                    }
+                }
+        }
+    }
+
+    return ans;
+}
+// Hashing Approch
+vector<vector<int>> ThreeSum(vector<int> nums)
 {
     vector<int> ans;
-    unordered_map<int, int> m;
+    int n = nums.size();
+    // a + b = -c
+    for (int i = 0; i < n; i++)
+    {
+        int tar = nums[i];
+        for (int j)
+    }
 }
 int main()
 {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    vector<int> result = twoSum(nums, target);
-    for (int val : result)
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    vector<vector<int>> result = threeSum(nums);
+
+    for (auto values : result)
     {
-        cout << val << ", ";
+        cout << values[1] << endl;
+        cout << values[2] << endl;
     }
+
     return 0;
 }
