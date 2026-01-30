3337b1f680809f9e0c6e8670ad97b71f2b762c78
Sakh-balti <f23bdats1e01005@iub.edu.pk>
2026-01-30 16:20:04 +0500

    insertion method

--- 2026/linklist/01_implement.cpp
+++ 2026/linklist/01_implement.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-// Implementation Of Linked List From Scratch
+// Blue print of node
 class Node
 {
+
 public:
     int data;
     Node *next;
@@ -28,7 +28,6 @@ public:
     void push_front(int val)
     {
         Node *newNode = new Node(val);
-
         if (head == NULL)
         {
             head = tail = newNode;
@@ -37,11 +36,107 @@ public:
         else
         {
             newNode->next = head;
+            head = newNode;
+        }
+    }
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    void insert(int val, int pos)
+    {
+        if (pos == 0)
+        {
+            push_front(val);
+            return;
+        }
+
+        Node *newNode = new Node(val);
+        Node *temp = head;
+
+        for (int i = 0; i < pos - 1; i++)
+        {
+            if (temp == NULL)
+            {
+                cout << "Invalid position";
+                delete newNode;
+                return;
+            }
+            temp = temp->next;
+        }
+
+        newNode->next = temp->next;
+        temp->next = newNode;
+
+        // If inserted at end, update tail
+        if (newNode->next == NULL)
+        {
+            tail = newNode;
+        }
+    }
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            cout << "ll are empty";
+            return;
+        }
+        Node *temp = head;
+        head = head->next;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+        delete temp;
+    }
+    void pop_back()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        if (head == tail)
+        {
+            delete head;
+            head = tail = NULL;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != tail)
+        {
+            temp = temp->next;
+        }
+        delete tail;
+        tail = temp;
+        tail->next = NULL;
+    }
+    void printValues()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
         }
     }
 };
-int main()
+main()
 {
+    List ll;
+    ll.push_back(12);
+    ll.push_back(32);
+    ll.push_back(90);
+    ll.pop_front();
+    ll.printValues();
 
     return 0;
 }
---
+++ 2026/linklist/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+
+    // Student *ptr = &st;
+    // Student *ptr = new Student;
+    // (*ptr).setName("sakhawat");
+    // (*ptr).getName();
