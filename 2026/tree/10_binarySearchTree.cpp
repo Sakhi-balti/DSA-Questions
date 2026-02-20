4ff633962fd3896e945bcaaf30a3fa21837e25a6
Sakh-balti <f23bdats1e01005@iub.edu.pk>
2026-02-20 23:13:49 +0500

    binary search perfoms on tree

---
+++ 2026/tree/10_binarySearchTree.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+Node *insert(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return new Node(val);
+    }
+    if (val < root->data)
+    {
+        root->left = insert(root->left, val);
+    }
+    else
+    {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+// This is special for binary search;
+Node *buildBinaryTree(vector<int> &nums)
+{
+    Node *root = NULL;
+    for (int val : nums)
+    {
+        root = insert(root, val);
+    }
+    return root;
+}
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+int main()
+{
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+    Node *root = buildBinaryTree(nums);
+    inorder(root);
+    return 0;
+}
